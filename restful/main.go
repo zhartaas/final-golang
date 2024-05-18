72a0e76b2fc1d6e473a70152ab77a99ed720b4e2
Ilyas <ilyas.nartay@gmail.com>
2024-05-18 14:33:25 +0300

    примерно так по идеи

--- restful/main.go
+++ restful/main.go
@@ -3,15 +3,25 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 )
+
+var jwtKey = []byte("100%forProject")
 
 type RegisterRequest struct {
 	Fullname string `json:"fullname"`
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+// Claims структура для JWT токена
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
 }
 
 func main() {
@@ -44,11 +54,29 @@ func handleRegister(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Здесь должна быть логика для сохранения пользователя в базу данных
+
+	// Создайте JWT токен
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := &Claims{
+		Username: req.Username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Возвращаем JWT токен в ответе
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	response := map[string]string{"message": "User registered successfully"}
+	response := map[string]string{"message": "User registered successfully", "token": tokenString}
 	json.NewEncoder(w).Encode(response)
-
 }
 
 func handleGet(w http.ResponseWriter, r *http.Request) {
