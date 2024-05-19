4af34ec260b5f80b1a56c91d0d0079d87323318c
zhartaas <satibaevzhartas@gmail.com>
2024-05-19 17:25:37 +0500

    SendMessage finally works

--- chat/chat.go
+++ chat/chat.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	db "finalProjectGolang/database"
 	"fmt"
+	"github.com/jackc/pgx/v4"
 )
 
 type server struct {
@@ -16,13 +17,36 @@ func (s *server) mustEmbedUnimplementedChatServiceServer() {
 }
 
 func NewChatServiceServer() *server {
-	return &server{}
+	db, err := db.CreateDatabase()
+	if err != nil {
+		return nil
+	}
+	return &server{DB: db}
 }
 
 func (s *server) SendMessage(ctx context.Context, msg *Message) (*SendResponse, error) {
+	fmt.Println(s)
+	chatID, err := s.DB.GetChat(msg.Receiver, msg.Sender)
 
+	if err != nil {
+		fmt.Println(err, pgx.ErrNoRows)
+		if err != pgx.ErrNoRows {
+			return &SendResponse{Status: "Error occured"}, err
+		}
+		chatID, err = s.DB.CreateChat(msg.Receiver, msg.Sender)
+
+		if err != nil {
+			return &SendResponse{Status: "error occured"}, err
+		}
+	}
+
+	res, err := s.DB.SendMessage(msg.Receiver, msg.Text, chatID)
+
+	if err != nil {
+		return &SendResponse{Status: "error occured"}, err
+	}
 	fmt.Printf("Received message from %s: %s\n", msg.Sender, msg.Text)
-	return &SendResponse{Status: "Message sent"}, nil
+	return &SendResponse{Status: fmt.Sprintf("Message sent, messageID:%v", res)}, nil
 }
 
 func (s *server) ReceiveMessages(req *ReceiveRequest, stream ChatService_ReceiveMessagesServer) error {
--- database/database.go
+++ database/database.go
@@ -21,11 +21,32 @@ type User struct {
 	Username string
 	Password string
 }
+
+type Chat struct {
+	chatID    int64
+	username1 string
+	username2 string
+}
+
 type Database struct {
 	DB *pgxpool.Pool
 }
 
 var db *Database
+
+func (db Database) SendMessage(username, text string, chatID int64) (int64, error) {
+	query := "INSERT INTO message(sender, text, chatID) VALUES ($1, $2, $3) RETURNING messageID"
+
+	var messageID int64
+
+	err := db.DB.QueryRow(context.Background(), query, username, text, chatID).Scan(&messageID)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return messageID, nil
+}
 
 func (db Database) GetUser(username string) (*User, error) {
 	var user User
@@ -42,6 +63,33 @@ func (db Database) GetUser(username string) (*User, error) {
 	}
 
 	return &user, nil
+}
+
+func (db Database) GetChat(username1, username2 string) (int64, error) {
+	query := "SELECT * FROM chat WHERE (username1 = $1 OR username1 = $2) AND (username2 = $3 OR username2 = $4)"
+
+	var chat Chat
+	err := db.DB.QueryRow(context.Background(),
+		query, username1, username2, username1, username2).Scan(&chat.chatID, &chat.username1, &chat.username2)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return chat.chatID, nil
+}
+
+func (db Database) CreateChat(username1, username2 string) (int64, error) {
+	query := "INSERT INTO chat (username1, username2) VALUES ($1, $2) RETURNING chatID"
+
+	var chatID int64
+
+	err := db.DB.QueryRow(context.Background(), query, username1, username2).Scan(&chatID)
+	if err != nil {
+		return 0, err
+	}
+
+	return chatID, nil
 }
 
 func (db Database) CreateUser(fullname, username, password string) error {
